c303ee00fa310a0f5b4631c0dffe12d6a9062c44
AayushTheGajjar181102 <96654867+AayushTheGajjar181102@users.noreply.github.com>
2022-12-28 15:54:54 +0530

    Update AddComponent.java

--- com.src/AddComponent.java
+++ com.src/AddComponent.java
@@ -12,7 +12,7 @@ interface AddComponent{
 	<T extends JComponent> void addSingle(ArrayList<T> list, String text);
 	<T extends JComponent> void addSingle(ArrayList<T> list, String text, boolean event);
 	
-	//Add Single List Components
+	//Add Multiple List Components
 	<T extends JComponent> void addMultiples(ArrayList<T> list, int total);
 	<T extends JComponent> void addMultiples(ArrayList<T> list, String... texts);
 	<T extends JComponent> void addMultiples(ArrayList<T> list, boolean event, String... texts);
